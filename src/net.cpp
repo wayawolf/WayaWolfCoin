b51faa6e2ec0cf321abbdd795899f8589a6e5f3d
Gavin Hurlbut <gjhurlbu@gmail.com>
2021-08-01 16:45:46 -0700

    Update upnpDiscover call to latest API

--- src/net.cpp
+++ src/net.cpp
@@ -1106,13 +1106,10 @@ void ThreadMapPort2(void* parg)
     struct UPNPDev * devlist = 0;
     char lanaddr[64];
 
-#ifndef UPNPDISCOVER_SUCCESS
-    /* miniupnpc 1.5 */
-    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
-    /* miniupnpc 1.6 */
+#if MINIUPNPC_API_VERSION == 17
+    /* miniupnpc 2.2.2 */
     int error = 0;
-    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 0, &error);
 #endif
 
     struct UPNPUrls urls;
